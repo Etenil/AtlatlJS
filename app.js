c6cd5ad80b6ebd38149e8a4433ef24bfce7b7b9d
Joe <joe@noisette.config>
2014-03-05 00:48:47 +0000

    Added simple ajax function and DOM lib.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var app = new atlatl.App();
 
 app.routes = {
-	'/': 'index'
+	'/': 'index',
+	'/toto': 'toto'
 };
 
-app.controllers.index = function() {
-	console.log('toto');
+app.controllers = {
+	index: function() {
+		HTML.query('#display').innerHTML = 'Index <a href="#/toto">toto</a>';
+	},
+	toto: function() {
+		var data = atlatl.ajax('test.html', function(data) {
+			HTML.query('#display').innerHTML = data;
+		});
+	}
 }
 
 app.serve();
--- src/atlatl.js
+++ src/atlatl.js
@@ -1,22 +1,75 @@
-var atlatl = {};
+var atlatl = {
+	App: function() {
+		var that = this;
 
-atlatl.App = function() {
-	this.routes = {};
-	this.controllers = {};
+		this.routes = {};
+		this.controllers = {};
+
+		this.serve = function() {
+			var route = window.location.hash.substring(1);
+
+			if(route == '') {
+				route = '/';
+			}
+
+			var controller = this.routes[route];
+
+			if(this.controllers[controller]) {
+				return this.controllers[controller]();
+			} else {
+				console.log("Unknown route '" + route + "'")
+			}
+		}
+
+		window.onhashchange = function(e) {
+			that.serve();
+			return false;
+		}
+	},
 	
-	this.serve = function() {
-		var route = window.location.hash.substring(1);
-		
-		if(route == '') {
-			route = '/';
-		}
-		
-		var controller = this.routes[route];
-		
-		if(this.controllers[controller]) {
-			return this.controllers[controller]();
-		} else {
-			console.log("Unknown route '" + route + "'")
-		}
+	ajax: function(url, callback) {
+		this.bindFunction = function(caller, object) {
+			return function() {
+            	return caller.apply(object, [object]);
+            };
+        };
+
+        this.stateChange = function(object) {
+            if(this.request.readyState==4) {
+                this.callbackFunction(this.request.responseText);
+			}
+        };
+
+        this.getRequest = function() {
+            if(window.ActiveXObject) {
+                return new ActiveXObject('Microsoft.XMLHTTP');
+            }
+			else if(window.XMLHttpRequest) {
+                return new XMLHttpRequest();
+			}
+            return false;
+        };
+
+        this.postBody = (arguments[2] || "");
+
+        this.callbackFunction = callback;
+        this.url=url;
+        this.request = this.getRequest();
+
+        if(this.request) {
+            var req = this.request;
+	        req.onreadystatechange = this.bindFunction(this.stateChange, this);
+
+	        if(this.postBody !== "") {
+	        	req.open("POST", url, true);
+	            req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	            req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	            req.setRequestHeader('Connection', 'close');
+	        } else {
+	            req.open("GET", url, true);
+	        }
+
+	        req.send(this.postBody);
+        }
 	}
 }
